ceee715d008c58e0efaf734950c01492ac5ecd5b
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2023-11-28 21:17:54 +0700

    timewarrior for polybar

---
+++ .config/polybar/bin/timewarrior.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+project=$(task _get "$(($(cat /tmp/tw_polybar_id)))".project)
+description=$(task _get "$(($(cat /tmp/tw_polybar_id)))".description)
+
+if timew >/dev/null 2>&1; then
+	printf "$(timew summary :day "$project" "$description" | awk '{print $NF}' | tail -2 | head -1)"
+else
+	printf "no tracking"
+fi
+
+if [ "${1}" = "--toggle" ]; then
+	if timew; then
+		task "$(cat /tmp/tw_polybar_id)" stop
+	else
+		task "$(cat /tmp/tw_polybar_id)" start
+	fi
+fi
