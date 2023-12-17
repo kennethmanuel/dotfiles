09e929d6e4f6dc8ce3c232fc498d9e512dc64bae
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2023-12-17 17:58:16 +0700

    better taskwarrior polybar integration with new hook

--- polybar/.config/polybar/bin/timewarrior.sh
+++ polybar/.config/polybar/bin/timewarrior.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-project=$(task _get "$(($(cat /tmp/tw_polybar_id)))".project)
-description=$(task _get "$(($(cat /tmp/tw_polybar_id)))".description)
-
-if timew >/dev/null 2>&1; then
-	printf "$(timew summary :day "$project" "$description" | awk '{print $NF}' | tail -2 | head -1)"
-else
+if [ "$(cat /tmp/tw_polybar_id)" == "" ]; then
 	printf "no tracking"
-fi
+else
+	project=$(task _get "$(cat /tmp/tw_polybar_id)".project)
+	description=$(task _get "$(cat /tmp/tw_polybar_id)".description)
 
-if [ "${1}" = "--toggle" ]; then
-	if timew; then
-		task "$(cat /tmp/tw_polybar_id)" stop
+	if timew >/dev/null 2>&1; then
+		printf "$(timew summary :day "$project" "$description" | awk '{print $NF}' | tail -2 | head -1)"
 	else
-		task "$(cat /tmp/tw_polybar_id)" start
+		printf "no tracking"
 	fi
+
+	if [ "${1}" = "--toggle" ]; then
+		if timew; then
+			task "$(cat /tmp/tw_polybar_id)" stop
+		else
+			task "$(cat /tmp/tw_polybar_id)" start
+		fi
+	fi
+
 fi
