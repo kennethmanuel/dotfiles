16ee0810a095d1e592e8690d90412701aae54f37
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2024-08-08 23:56:15 +0700

    update

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -19,3 +19,20 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "delete to plus regi
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "delete to plus register" })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+if vim.g.vscode then
+  -- VSCode extension
+  -- vscode-harpoon.addEditor
+  vim.keymap.set("n", "<leader>hh", "<Cmd>lua require('vscode').call('vscode-harpoon.addEditor')<CR>")
+  -- VSCode Harpoon: Go to editor [1-9] (vscode-harpoon.gotoEditor[1-9])
+  vim.keymap.set("n", "<leader>1", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor1')<CR>")
+  vim.keymap.set("n", "<leader>2", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor2')<CR>")
+  vim.keymap.set("n", "<leader>3", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor3')<CR>")
+  vim.keymap.set("n", "<leader>4", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor4')<CR>")
+  -- VSCode Harpoon: Edit Editors (vscode-harpoon.editEditors)
+  vim.keymap.set("n", "<leader>he", "<Cmd>lua require('vscode').call('vscode-harpoon.editEditors')<CR>")
+  -- SCode Harpoon: Editor Quick Pick (vscode-harpoon.editorQuickPick)
+  vim.keymap.set("n", "<leader>h<leader>", "<Cmd>lua require('vscode').call('vscode-harpoon.editorQuickPick')<CR>")
+else
+  -- ordinary Neovim
+end
