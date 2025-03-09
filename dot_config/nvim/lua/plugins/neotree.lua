8426a8158867d10fca7417bf089e46679463805a
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-03-09 10:40:11 +0700

    nvim thimg

--- dot_config/nvim/lua/plugins/neotree.lua
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -3,17 +3,19 @@ return {
     opts = {
         source_selector = {
             winbar = true,
-            statusline = false,
+            statusline = true,
         },
         filesystem = {
             filtered_items = {
                 visible = true,
             },
         },
+        sources = { "filesystem", "buffers", "git_status", "document_symbols" },
     },
     keys = {
         { "<leader>we", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
         { "<leader>e", false },
         { "<leader>E", false },
+        { "<leader>ws", "<cmd>Neotree document_symbols<cr>" },
     },
 }
